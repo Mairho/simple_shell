3d3e22c281eadfad39df6d49d39e3cfb2771b8e2
Mairho <111153409+Mairho@users.noreply.github.com>
2022-11-16 13:55:03 +0100

    Update _getenv.c

--- _getenv.c
+++ _getenv.c
@@ -6,14 +6,14 @@
   *
   * Return: The content of the environment variable
   */
-char *_getenv(const char *name)
+char *_getenv(info _t *, const char *)
 {
 	int i = 0;
 	char *env_var = NULL;
 
 	while (environ[i])
 	{
-		if (_strncmp(name, environ[i], _strlen(name)) == 0)
+		if (_strcmp(name, environ[i], _strlen(name)) == 0)
 		{
 			env_var = _strdup(environ[i]);
 			while (*env_var != '=')
